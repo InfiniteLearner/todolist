7085ac31e4df6d47f5fa2a8cfaba8e0d4eba2977
InfiniteLearner <cyprien.guenard.pro@protonmail.com>
2022-12-27 17:09:49 +0100

    First version of Todo list v1.0 (no styling)

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
-const e = require("express");
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
+const STORAGE_FILE_NAME = "items" ;
+const SPLIT_CARACTER = "\n" ;
+
+var items = [];
+
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended : true}));
+
+app.post("/", (req, res) => {
+    var item = SPLIT_CARACTER + req.body.newItem ;
+    var options = {
+        flag : "a"
+    };
+
+    fs.writeFileSync(STORAGE_FILE_NAME, item, options);
+
+    res.redirect("/");
+})
 
 app.get("/", (req, res) => {
     var today = new Date();
-    var day ;
 
-    switch(today.getDay()){
-        case 0 :
-            day = "Sunday" ;
-            break ;
-        case 1 :
-            day = "Monday" ;
-            break ;
-        case 2 :
-            day = "Tuesday" ;
-            break ;
-        case 3 :
-            day = "Wednesday";
-            break ;
-        case 4 : 
-            day = "Thursday" ;
-            break ;
-        case 5 :
-            day = "Friday" ;
-            break ;
-        case 6 :
-            day = "Saturday";
-            break ;
-        default :
-            console.error("Unknow day : " + today.getDay());
-    }
+    var options = {
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    };
 
-    res.render("list", {day : day});
+    var day = today.toLocaleDateString("fr-FR", options);
 
+    items = fs.readFileSync(STORAGE_FILE_NAME).toString().split(SPLIT_CARACTER);
+
+    res.render("list", {day : day, items : items});
 })
 
 app.listen(3000, () => {
