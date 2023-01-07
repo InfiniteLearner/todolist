f1b565a7c5169462fc7ed063865f678d5980a9bb
Cyprien Guenard <cyprien.guenard.pro@protonmail.com>
2023-01-07 13:07:05 +0100

    V2.2
    Add custom list

--- src/app.ts
+++ src/app.ts
@@ -19,14 +19,11 @@ app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
 
+const DEFAULT_CATEGORY = "default" ;
+
 /*--------------------*/
 
 /* Database objects */
-
-enum Category {
-    Default = "DEFAULT",
-    Work = "WORK"
-};
 
 interface IItem {
     category : string ;
@@ -51,24 +48,25 @@ const Item = model<IItem>("Item", itemSchema);
 /* POST */
 
 app.post("/", (req: Request, res: Response) => {
+    res.redirect("/list/" + DEFAULT_CATEGORY);
+})
+
+app.post("/list/:category", (req: Request, res: Response) => {
     console.log("POST request");
 
     const item : string = req.body.newItem ;
-    let redirectPath : string ;
+    //if this is the default list, we discard the title of the list
+    const category : string = req.params.category ;
+    console.log("Category : " + category);
+
+    let redirectPath : string = (category === DEFAULT_CATEGORY) ? "/" : "/" + category ;
     let mongooseItem: Document<unknown, any, IItem> ;
 
     console.log("Creating item");
-
-    if(req.body.list === Category.Work.toLocaleLowerCase()){
-        mongooseItem = new Item({category: Category.Work, description: item});
-        redirectPath = "/" + Category.Work.toLocaleLowerCase();
-    }else{
-        mongooseItem= new Item({category: Category.Default, description: item});
-        redirectPath = "/" ;
-    }
+    mongooseItem = new Item({category: category, description: item});
+    
 
     console.log("Saving item");
-
     mongooseItem.save((err: CallbackError, item: Document<unknown, any, IItem> ) => {
         if(err){
             throw err ;
@@ -90,9 +88,8 @@ app.post("/delete", (req: Request, res: Response) => {
         if(err){
             throw err ;
         }else{
-            const redirectPath : string = (item.category === Category.Work) ? "/" + Category.Work.toLocaleLowerCase() : "/" ;
+            const redirectPath : string = (item.category === DEFAULT_CATEGORY) ? "/"  : "/" + item.category ;
             res.redirect(redirectPath);
-
         }
     });
 })
@@ -106,20 +103,21 @@ app.get("/", (req: Request, res: Response) => {
    
     const day : string = getDate();
 
-    findAndRender(day, Category.Default, res);
+    findAndRender(day, DEFAULT_CATEGORY, res);
 })
 
-app.get("/" + Category.Work.toLocaleLowerCase(), (req: Request, res: Response) => {
-    console.log("GET request at /" + Category.Work.toLocaleLowerCase())
+app.get("/:category", (req: Request, res: Response) => {
+    const category: string = req.params.category;
+    console.log("GET request at /" + category);
 
-    findAndRender(Category.Work.toLocaleLowerCase(), Category.Work, res);
+    findAndRender(category, category, res);
 })
 
 /*---------------*/
 
 /* Helping functions */
 
-function findAndRender(title: string, category: Category, res: Response): void{
+function findAndRender(title: string, category: string, res: Response): void{
 
     Item.find({category: category}, (err: CallbackError, items: Array<IItem>) => {
         if(err){
@@ -127,7 +125,7 @@ function findAndRender(title: string, category: Category, res: Response): void{
         }else{
             console.log(category + " items have been successfuly loaded");
             console.log("Rendering items...");
-            res.render("list", {listTitle : title, items : items});
+            res.render("list", {listTitle : title, items : items, category: category});
         }
     })
 }
