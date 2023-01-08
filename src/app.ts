3ae491b50624f5ab3859f6ede0576a2b0813194a
Cyprien Guenard <cyprien.guenard.pro@protonmail.com>
2023-01-08 05:15:08 +0100

    V2.1.1
    Correct bug on delete items
    Correct bug on css display

--- src/app.ts
+++ src/app.ts
@@ -26,15 +26,10 @@ const DEFAULT_CATEGORY = "default" ;
 /* Database objects */
 
 interface IItem {
-    category : string ;
     description : string ;
 }
 
 const itemSchema = new Schema<IItem>({
-    category: {
-        type: String,
-        required: [true, "The item should be classified in a given category"]
-    },
     description: {
         type: String,
         required: [true, "You need to give a description for the item"]
@@ -42,6 +37,23 @@ const itemSchema = new Schema<IItem>({
 });
 
 const Item = model<IItem>("Item", itemSchema);
+
+interface IList {
+    name: string ;
+    items: IItem[]
+}
+
+const listSchema = new Schema<IList>({
+    name: {
+        type: String,
+        required: [true, "The list need to have a name"]
+    },
+    items: [
+       itemSchema
+    ]
+})
+
+const List = model<IList>("List", listSchema);
 
 /*------------------------*/
 
@@ -57,39 +69,90 @@ app.post("/list/:category", (req: Request, res: Response) => {
     const item : string = req.body.newItem ;
     //if this is the default list, we discard the title of the list
     const category : string = req.params.category ;
-    console.log("Category : " + category);
-
-    let redirectPath : string = (category === DEFAULT_CATEGORY) ? "/" : "/" + category ;
-    let mongooseItem: Document<unknown, any, IItem> ;
-
     console.log("Creating item");
-    mongooseItem = new Item({category: category, description: item});
-    
+    let redirectPath : string = (category === DEFAULT_CATEGORY) ? "/" : "/list/" + category ;
+    let mongooseItem: Document<unknown, any, IItem> & IItem = new Item({category: category, description: item});
 
-    console.log("Saving item");
-    mongooseItem.save((err: CallbackError, item: Document<unknown, any, IItem> ) => {
+    List.findOne({name: category}, (err : CallbackError, list: Document<unknown, any, IList> & IList) => {
+
+        let listToSave : Document<unknown, any, IList> & IList ;
+
         if(err){
-            throw err ;
+            console.log(err); 
+            throw err; ;
+        }else if(list === null){
+            console.log("Creating new list");
+            listToSave = new List({name: category, items: [mongooseItem]});
         }else{
-            if(item === mongooseItem){
-                res.redirect(redirectPath);
-            }else{
-                throw new Error("Item didn't save " + mongooseItem);
-            }
+            console.log("Updating existing list");
+            list.items.push(mongooseItem);
+            listToSave = list ;
         }
-    });
+
+        listToSave.save((err: CallbackError, savedList: IList) => {
+            if(err){
+                console.log(err); 
+                throw err; ;
+            }else if(listToSave !== savedList){
+                throw new Error("List hasn't been saved : " + savedList.name);
+            }else{
+                console.log("Saving item");
+                mongooseItem.save((err: CallbackError, item: Document<unknown, any, IItem> ) => {
+                    if(err){
+                        console.log(err); 
+                        throw err; ;
+                    }else{
+                        if(item === mongooseItem){
+                            res.redirect(redirectPath);
+                        }else{
+                            throw new Error("Item didn't save " + mongooseItem);
+                        }
+                    }
+                });
+
+            }
+        })
+    })
+
 })
 
 app.post("/delete", (req: Request, res: Response) => {
     const id : string = req.body.checkbox ;
     console.log("DELETE request for " + id);
 
-    Item.findByIdAndDelete({_id: id}, (err: CallbackError, item: IItem) => {
+    Item.findOneAndDelete({_id: id}, (err: CallbackError, item: IItem) => {
         if(err){
-            throw err ;
-        }else{
-            const redirectPath : string = (item.category === DEFAULT_CATEGORY) ? "/"  : "/" + item.category ;
-            res.redirect(redirectPath);
+            console.log(err); 
+            throw err; ;
+        }else if(!item){
+            throw new Error("No item with the given id was found : " + id);
+        }
+        else{
+            console.log("Deleting in list");
+            List.findOne({"items.description": item.description}, (err: CallbackError, list: Document<unknown, any, IList> & IList) => {
+                if(err){
+                    console.log(err); 
+                    throw err; ;
+                }else if(!list){
+                    throw new Error("The item was not in any list : " + item.description);
+                }else{
+                    list.items = list.items.filter(it => it.description !== item.description);
+
+                    list.save((err: CallbackError, savedList: IList) => {
+                        if(err){
+                            console.log(err); 
+                            throw err; ;
+                        }else if(list !== savedList){
+                            throw new Error("List hasn't been saved : " + savedList.name);
+                        }else{
+                           console.log("List updated");
+                           const redirectPath = (list.name === DEFAULT_CATEGORY) ? "/" : "/list/" + list.name ;
+                           res.redirect(redirectPath);
+                        }
+                    })
+                   
+                }
+            })
         }
     });
 })
@@ -103,10 +166,11 @@ app.get("/", (req: Request, res: Response) => {
    
     const day : string = getDate();
 
+
     findAndRender(day, DEFAULT_CATEGORY, res);
 })
 
-app.get("/:category", (req: Request, res: Response) => {
+app.get("/list/:category", (req: Request, res: Response) => {
     const category: string = req.params.category;
     console.log("GET request at /" + category);
 
@@ -119,19 +183,29 @@ app.get("/:category", (req: Request, res: Response) => {
 
 function findAndRender(title: string, category: string, res: Response): void{
 
-    Item.find({category: category}, (err: CallbackError, items: Array<IItem>) => {
+    List.findOne({name: category}, (err: CallbackError, list: IList) => {
         if(err){
-            throw err ;
+            console.log(err); throw err; ;
+        }else if(!list){
+            let listToSave : Document<unknown, any, IList> & IList ;
+            listToSave = new List({name: category, items: []});
+
+            listToSave.save((err: CallbackError, savedList: IList) => {
+                if(err){
+                    console.log(err); 
+                    throw err; ;
+                }else if(listToSave !== savedList){
+                    throw new Error("List hasn't been saved : " + savedList.name);
+                }else{
+                    res.render("list", {listTitle : title, items : savedList.items, category: savedList.name});
+                }
+            })
         }else{
             console.log(category + " items have been successfuly loaded");
             console.log("Rendering items...");
-            res.render("list", {listTitle : title, items : items, category: category});
+            res.render("list", {listTitle : title, items : list.items, category: list.name});
         }
     })
-}
-
-function deleteItem(item: string){
-    console.log(item);
 }
 
 /*--------------*/
